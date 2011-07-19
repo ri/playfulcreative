75104207247f1147211b374e02367283aecae437
ri <ri@playfulcreative.com>
2011-07-19 15:56:13 -0500

    updated interaction project image to have loader and to load after imagesloaded

--- site/js/interaction.js
+++ site/js/interaction.js
@@ -1,22 +1,25 @@
-$(window).load(function() {
+$(document).ready(function() {
 	$('#pixemo-images').AccordionImageMenu({
 	  'closeDim'  : 240,
 	  'openDim'   : 400,
 	  'height'    : 300,
-	  'border'    : 0
+	  'border'    : 0,
+	  'color'     : "none"
 	});
 	
 	$('#vm-images').AccordionImageMenu({
 	  'closeDim'  : 320,
 	  'openDim'   : 400,
 	  'height'    : 300,
-	  'border'    : 0
+	  'border'    : 0,
+	  'color'     : "none"
 	});
 	
 	$('#envision-images').AccordionImageMenu({
 	  'closeDim'  : 240,
 	  'openDim'   : 400,
 	  'height'    : 300,
-	  'border'    : 0
+	  'border'    : 0,
+	  'color'     : "none"
 	});
 });
